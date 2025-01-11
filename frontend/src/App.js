f743f4362b001cf47a1c8213cca2483fd40c2f16
Austen Leow <austen.leow.2021@scis.smu.edu.sg>
2025-01-11 13:31:54 +0800

    set up frontend routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,54 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 import LandingPage from "./components/pages/landingPage";
 import Login from "./components/pages/loginPage.js"
 
+const AuthPublicGuard = () => {
+  const authUser = getCurrentUser();
+
+  return authUser ? (
+    <Outlet context={{ isLoggedIn: true }} />
+  ) : (
+    <Outlet context={{ isLoggedIn: false }} />
+  );
+};
+
+const AuthPrivateGuard = () => {
+  const authUser = getCurrentUser();
+
+  return authUser ? <Outlet /> : <Navigate to={"/login"} replace />;
+};
+
+const getCurrentUser = () => {
+  return localStorage.getItem("username");
+};
+
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-      <Login />
-    </div>
+    <React.StrictMode>
+        <BrowserRouter>
+
+         <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route exact path="/login" element={<Login />} />
+  
+
+            <Route element={<AuthPublicGuard />}>
+
+            </Route>
+
+            <Route element={<AuthPrivateGuard />}>
+
+            </Route>
+          </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
   );
 }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 export default App;
