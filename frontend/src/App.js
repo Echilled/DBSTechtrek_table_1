0ceffbe3c3f7dabe912023821536392644cdb2da
Max Tan <e0772757@u.nus.edu>
2025-01-11 12:17:14 +0800

    Add Navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      {/* Your page content goes here */}
+      <h1 className="text-3xl font-bold">Test</h1>
+    </Layout>
   );
 }
 
